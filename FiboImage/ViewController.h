9ae2a002030f338fb134a599100561be95b7c1de
Andrei Puni <puni.andrei23@gmail.com>
2013-09-11 02:51:30 +0300

    Initial Commit

---
+++ FiboImage/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FiboImage
+//
+//  Created by Andrei on 9/11/13.
+//  Copyright (c) 2013 Andrei Puni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ FiboImage/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  FiboImage
+//
+//  Created by Andrei on 9/11/13.
+//  Copyright (c) 2013 Andrei Puni. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
